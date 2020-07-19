82e073bb6892849af134e44ca866bc32c2deb544
asheemstha <asheem.shrestha01@gmail.com>
2020-07-20 00:26:52 +0545

    Updates

---
+++ precache-manifest.6f808bd0a78e52a892487d66ae4588b9.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae217ec2452be372dce9203a428c3a03",
+    "url": "/bechdey/index.html"
+  },
+  {
+    "revision": "2b535010a7413881f05d",
+    "url": "/bechdey/static/css/main.836970cb.chunk.css"
+  },
+  {
+    "revision": "44ca8a7b20a0868a1d8c",
+    "url": "/bechdey/static/js/2.37e9cd82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bechdey/static/js/2.37e9cd82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b535010a7413881f05d",
+    "url": "/bechdey/static/js/main.3b87b7c5.chunk.js"
+  },
+  {
+    "revision": "745a0fac9b21b2ec15d6",
+    "url": "/bechdey/static/js/runtime-main.1767e28c.js"
+  },
+  {
+    "revision": "5f2ce7df2a2e8570f4c32a44414df347",
+    "url": "/bechdey/static/media/Inter-Black.5f2ce7df.ttf"
+  },
+  {
+    "revision": "91e5aee8f44952c0c14475c910c89bb8",
+    "url": "/bechdey/static/media/Inter-Bold.91e5aee8.ttf"
+  },
+  {
+    "revision": "bd9525f1099e9f5845f6aef2956e9fb8",
+    "url": "/bechdey/static/media/Inter-ExtraBold.bd9525f1.ttf"
+  },
+  {
+    "revision": "909744bbb5a7ede41ce522a1507e952c",
+    "url": "/bechdey/static/media/Inter-ExtraLight.909744bb.ttf"
+  },
+  {
+    "revision": "6ffbefc66468b90d7af1cbe1e9f13430",
+    "url": "/bechdey/static/media/Inter-Light.6ffbefc6.ttf"
+  },
+  {
+    "revision": "5ff1f2a9a78730d7d0c309320ff3c9c7",
+    "url": "/bechdey/static/media/Inter-Medium.5ff1f2a9.ttf"
+  },
+  {
+    "revision": "515cae74eee4925d56e6ac70c25fc0f6",
+    "url": "/bechdey/static/media/Inter-Regular.515cae74.ttf"
+  },
+  {
+    "revision": "ec60b23f3405050f546f4765a9e90fec",
+    "url": "/bechdey/static/media/Inter-SemiBold.ec60b23f.ttf"
+  },
+  {
+    "revision": "35b7cf4cc47ac526b745c7c29d885f60",
+    "url": "/bechdey/static/media/Inter-Thin.35b7cf4c.ttf"
+  },
+  {
+    "revision": "0573b9231a8316427ad6e751b52e87a4",
+    "url": "/bechdey/static/media/Poppins-Black.0573b923.ttf"
+  },
+  {
+    "revision": "a3e0b5f427803a187c1b62c5919196aa",
+    "url": "/bechdey/static/media/Poppins-Bold.a3e0b5f4.ttf"
+  },
+  {
+    "revision": "544fa4f2678a8285eb88b8dfe503c90c",
+    "url": "/bechdey/static/media/Poppins-ExtraBold.544fa4f2.ttf"
+  },
+  {
+    "revision": "86a2f13e91ac85080ebaeaab9463b9f1",
+    "url": "/bechdey/static/media/Poppins-ExtraLight.86a2f13e.ttf"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/bechdey/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/bechdey/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/bechdey/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/bechdey/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "25cd0f688f815bc4f6ac2b71eb6278ba",
+    "url": "/bechdey/static/media/Poppins-Thin.25cd0f68.ttf"
+  },
+  {
+    "revision": "7e3eae03cc63672b335ebbe576c3310b",
+    "url": "/bechdey/static/media/bechdey-logo-dark.7e3eae03.png"
+  },
+  {
+    "revision": "b2d6510428ad33823191bc451621d1a8",
+    "url": "/bechdey/static/media/manrope-bold.b2d65104.otf"
+  },
+  {
+    "revision": "06d638c060302ca0b55d6dfba20686c8",
+    "url": "/bechdey/static/media/manrope-extrabold.06d638c0.otf"
+  },
+  {
+    "revision": "78cfdc6416d964aad49d1d9012155f55",
+    "url": "/bechdey/static/media/manrope-light.78cfdc64.otf"
+  },
+  {
+    "revision": "313854186f7ab99de7ff2cd3a229435e",
+    "url": "/bechdey/static/media/manrope-medium.31385418.otf"
+  },
+  {
+    "revision": "93bcdc811b863241e3c2f2472764c5c5",
+    "url": "/bechdey/static/media/manrope-regular.93bcdc81.otf"
+  },
+  {
+    "revision": "809e36f11861afe527bda8afa0cce855",
+    "url": "/bechdey/static/media/manrope-semibold.809e36f1.otf"
+  },
+  {
+    "revision": "acdc45e42b42c23a18d85d921ab2cfca",
+    "url": "/bechdey/static/media/manrope-thin.acdc45e4.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bechdey./precache-manifest.4d05d8e3dcbb9dd84144c2fcf9a3f670.js"
+  "/bechdey/precache-manifest.6f808bd0a78e52a892487d66ae4588b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bechdey./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bechdey/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
